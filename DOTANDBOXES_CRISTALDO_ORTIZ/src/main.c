dc4b51f3d94b7f4c0ae56ec3e3b4b0d2dace1c9d
danusitacm <danielacristaldomo@gmail.com>
2021-09-22 13:25:26 -0400

    aguegue la funcion crear tablero imprimir tablero e inicilizar tablero

--- DOTANDBOXES_CRISTALDO_ORTIZ/src/main.c
+++ DOTANDBOXES_CRISTALDO_ORTIZ/src/main.c
@@ -11,6 +11,35 @@ int check_size_board(int number){
 	}
 	return number;
 }
+int **create_board(int size){
+	//Crea el tablero
+	int **array;
+	array=calloc(size,sizeof(int *));
+	for(int i=0;i<size;i++){
+		array[i]=calloc(size,sizeof(int *));
+	}
+	return array;
+}
+void initialize_board(int **array){
+	//Iniciliza el tablero para el juego
+	for(int i=0;i<boardSize;i++){
+		for(int j=0;j<boardSize;j++){
+			if(i%2==0 && j%2==0){
+				array[i][j]=DOT;
+			}
+		}
+	}
+}
+void print_board(int **array){
+	//Imprime el tablero
+	for(int i=0;i<boardSize;i++){
+		for(int j=0;j<boardSize;j++){
+			if(array[i][j]==DOT){
+				printf(" * ");
+			}
+		}printf("\n");
+	}
+}
 int valid_move(int initialRow,int initialColumn,int finalRow, int finalColumn){
 	//Verifica si hace un movimiento valido (vertical y horizontal)
 	//#Si la resta entre las columnas es 0 es un movimiento vertical
@@ -28,7 +57,7 @@ int valid_move(int initialRow,int initialColumn,int finalRow, int finalColumn){
 		}
 	}
 }
-void move_player(){
+void move_player(int **board){
 	//mensaje para ethel: te dejo esta funcion para que mejores
 	int initialRow,initialColumn,finalRow,finalColumn;
 	printf("\nIntroduzca la posicion inicial fila: ");
@@ -49,6 +78,10 @@ int main(void) {
 	printf("\nIntroduzca el tamaño del tablero: ");
 	scanf("%i",&boardSize);
 	boardSize=check_size_board(boardSize);
-	move_player();
+	boardSize=boardSize+(boardSize-1);
+	int **board=create_board(boardSize);
+	initialize_board(board);
+	print_board(board);
+	move_player(board);
 	return EXIT_SUCCESS;
 }
