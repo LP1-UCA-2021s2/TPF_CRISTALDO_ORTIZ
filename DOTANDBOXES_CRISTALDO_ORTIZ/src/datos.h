6e2d4566fa7f2e4e2624885b70489d63cc8eeffa
danusitacm <danielacristaldomo@gmail.com>
2021-09-21 20:46:15 -0400

    agregue varias funciones y cree el tablero

--- DOTANDBOXES_CRISTALDO_ORTIZ/src/datos.h
+++ DOTANDBOXES_CRISTALDO_ORTIZ/src/datos.h
@@ -9,6 +9,7 @@
 #define SRC_DATOS_H_
 #include<stdlib.h>
 #include<stdio.h>
+#define TRUE 1
+#define FALSE 0
 int boardSize;
-
 #endif /* SRC_DATOS_H_ */
--- DOTANDBOXES_CRISTALDO_ORTIZ/src/main.c
+++ DOTANDBOXES_CRISTALDO_ORTIZ/src/main.c
@@ -6,14 +6,52 @@ int random_number(int max,int min){
 int check_size_board(int number){
 	//Verifica el tamaño que el usuario ingresa para el tablero
 	while(number<3 || number>15){
-		printf("Error, introduzca un valor entre 3 y 15: ");
+		printf("\nError, introduzca un valor entre 3 y 15: ");
 		scanf("%i",&number);
 	}
 	return number;
+}
+int valid_move(int initialRow,int initialColumn,int finalRow, int finalColumn){
+	//Verifica si hace un movimiento valido (vertical y horizontal)
+	//#Si la resta entre las columnas es 0 es un movimiento vertical
+	//#Si la resta entre las filas es 0 es un movimiento horizontal
+	//Para tener en cuenta la diferencia del movimiento no debe ser mayor a 1
+	int columnDifference=initialColumn-finalColumn;
+	int rowDifference=initialRow-finalRow;
+	if(columnDifference==0 && abs(rowDifference)==1){
+		return TRUE;
+	}else{
+		if(rowDifference==0 && abs(columnDifference)==1){
+			return TRUE;
+		}else{
+			return FALSE;
+		}
+	}
+}
+void move_player(){
+	//mensaje para ethel: te dejo esta funcion para que mejores
+	int initialRow,initialColumn,finalRow,finalColumn;
+	printf("\nIntroduzca la posicion inicial fila: ");
+	scanf("%i",&initialRow);
+	printf("\nIntroduzca la posicion inicial columna: ");
+	scanf("%i",&initialColumn);
+	printf("\nIntroduzca la posicion final fila: ");
+	scanf("%i",&finalRow);
+	printf("\nIntroduzca la posicion final columna: ");
+	scanf("%i",&finalColumn);
+	if(valid_move(initialRow,initialColumn,finalRow,finalColumn)){
+		printf("\nMovimiento valido");
+	}else{
+		printf("\nMovimiento invalido");
+	}
 }
 int main(void) {
 	printf("\nIntroduzca el tamaño del tablero: ");
 	scanf("%i",&boardSize);
 	boardSize=check_size_board(boardSize);
+	boardSize=boardSize+(boardSize-1);
+	printf("%i",boardSize);
+	int board[boardSize][boardSize];
+	move_player();
 	return EXIT_SUCCESS;
 }
